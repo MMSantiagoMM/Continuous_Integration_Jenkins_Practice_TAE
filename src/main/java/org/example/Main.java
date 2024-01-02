0a35e9186c96cd9c15fc273acfac0e67f4cebada
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-02 14:22:51 -0500

    Printing environmental variable

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String x = System.getenv("variableName");
+        System.out.println(x);
+    }
+}
