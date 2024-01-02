b311e916f51235c5973ccc1ac179676030fc9540
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-02 18:57:42 -0500

    changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String variableName = System.getenv("variableName");
-        System.out.println(variableName);
+        String x = System.getenv("variableName");
+        System.out.println(x);
         System.out.println("Helloooooo");
     }
 }
