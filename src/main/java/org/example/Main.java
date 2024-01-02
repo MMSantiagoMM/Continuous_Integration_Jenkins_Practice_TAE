240a7e0171067ac85f19e6ef7f7721ef94f785a7
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-02 18:47:13 -0500

    Adding printed message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         String variableName = System.getenv("variableName");
         System.out.println(variableName);
+        System.out.println("Helloooooo");
     }
 }
