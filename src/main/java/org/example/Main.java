6ac81ea24a1d4139b7205eeef1403752c129b027
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-02 19:05:23 -0500

    Changing variable name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String x = System.getenv("variableName");
+        String x = System.getenv("NAME");
         System.out.println(x);
         System.out.println("Helloooooo");
     }
