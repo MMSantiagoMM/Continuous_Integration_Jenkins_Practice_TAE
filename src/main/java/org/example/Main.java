09fe7696a544102ecc671a7e5abab80f30e3eca2
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-02 19:18:37 -0500

    Hidden variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String x = System.getenv("NAME");
-        System.out.println(x);
+        //String x = System.getenv("NAME");
+        //System.out.println(x);
         System.out.println("Helloooooo");
     }
 }
