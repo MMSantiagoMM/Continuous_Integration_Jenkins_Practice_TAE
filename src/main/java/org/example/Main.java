64cd55b9f9ca297c7ae9747f771337f069065eda
Santiago Mosquera Mapura <santiagomosquera2@gmail.com>
2024-01-03 14:14:11 -0500

    renaming variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        //String x = System.getenv("NAME");
-        //System.out.println(x);
+        String NAME = System.getenv("NAME");
+        System.out.println(NAME);
         System.out.println("Helloooooo");
     }
 }
